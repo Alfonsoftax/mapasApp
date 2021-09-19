b83644ed0bcb64753567ae3fa6a78a8285a2e567
Alfonsoftax <alfonsoftax@gmail.com>
2021-09-19 17:35:47 +0200

    fin seccion de mapas

---
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+@Component({
+  selector: 'app-full-screen',
+  templateUrl: './full-screen.component.html',
+  styles: [
+    `
+    #mapa{
+      width: 100%;
+      height:100%;
+    }
+    `
+  ]
+})
+export class FullScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    var map = new mapboxgl.Map({
+    container: 'mapa', //id del div del mapa
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [ -5.988522746802072,37.37501342486401],
+    zoom:16
+});
+  }
+
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoiYWxmb25zb2Z0YXgiLCJhIjoiY2t0b21mM2Q2MGVjNDJvb3hpcWR5cHcxciJ9.nFfwsaY8jHoksnUUom6v_g'
 };
